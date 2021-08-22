1a509ffc11ef8e82f4949141f877b65564a39f2d
Park.Lu <park.lu@nogle.com>
2021-08-22 14:10:31 +0800

    init

--- readme.md
+++ readme.md
@@ -1,3 +1,43 @@
-# Nodejs + Typescript Bioplate
+# Din-Ben-Ton 2.0 Draft 
 
-[Reference](https://github.com/afteracademy/nodejs-backend-architecture-typescript)
+## APIs
+
+### 管理
+
+admin打算都用POST MAN操作, 有機會再做介面
+
+上傳/修改 廠商資料 POST/PATCH /admin/comapny
+
+上傳/修改 菜單/便當資料 POST/PATCH /admin/comapny/:id/menu
+- 原則上上傳整個菜單, 可以不刪便當, 改active就可
+
+取得目前使用者群資料 GET /admin/users
+
+新增/更新使用者資料 POST/PATCH /user/:id
+
+### 使用
+
+登入 POST /login
+
+新增使用者資料 POST /user
+
+登出 POST /logout
+
+列廠商清單 GET /company
+
+點擊進入廠商菜單列表 GET/company/:id/menu
+
+發起今天要訂的便當廠商 PATCH /company/:id/activity
+
+使用者選擇要訂的便當(下訂)
+
+使用者選擇要訂的便當的付款方式  POST /order/:userId
+ - TODO: ine pay 要怎麼處理的細節
+
+訂單截止 - 顯示成功或失敗(不到人數免運)
+
+目前所有訂單列表check list GET /order
+
+
+
+
---
+++ src/schema.ts
@@ -0,0 +1,56 @@
+// 便當店
+export interface Company  {
+  id: number,
+  name: string,
+  address: string,
+  minOrderNum: number, // 最少免運訂購數量
+  orderBefore: string, // 幾點前需要下訂 ex: 10:30PM
+  active: boolean, // 是不是今天所發起要訂的便當
+  menu: Bento[],
+  order?: Order, //訂單應該不用紀錄, 隔天下單的就重新塞
+}
+
+export interface Bento {
+  id: number,
+  name: string, // 排骨飯
+  price: number,
+  active: boolean,
+}
+
+// 本公司員工
+export interface User {
+  userId: number,
+  nogleId: string, // 看有無需要
+  email: string,
+  nickName: string,
+  walletAddress?: string, // 錢包地址
+  acceptPayMethod: PayMethod[], // 預設 cash, 當發起者的時候, 可以接受別人用啥付款
+  linePayId?: string,
+}
+
+// 今天要下訂的內容
+export interface Order {
+  orderDate: string,
+  companyId: number, // 便當店id
+  activeUserId?: number, // 發起人的id
+  list: WhoOrderWhat[],
+}
+
+// 付款方式
+enum PayMethod {
+  Cash,
+  LinePay,
+  Wallet, // 數位貨幣
+  Other, // 其他支付方式
+}
+
+// 誰訂了什麼
+export interface WhoOrderWhat {
+  userId: number,
+  nickName: string,
+  bentoId: number,
+  bentoName: string,
+  paid: boolean,
+  payMethod?: PayMethod,
+  gotThis: boolean, // 是否已經拿到便當, 用來做給發起者檢查打勾用
+}
